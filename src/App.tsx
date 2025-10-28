1735b2483f73fb2216c7813c7a128dfc61b695fd
onyedika-anagha <paschalanagha@gmail.com>
2025-10-28 14:54:39 +0100

    refactor: streamlined index.html structure, integrated PWA install prompt in App component, and update dark theme background color

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from "react-router";
 import router from "./routes";
+import PWAInstallPrompt from "./components/install-prompt";
 
 function App() {
   return (
     <>
       <RouterProvider router={router} />
+      <PWAInstallPrompt />
     </>
   );
 }
